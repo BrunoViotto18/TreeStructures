9357ce64303298d123cade6bb0ae412b97246cc4
BruhhnoV <brunoviottosantos@gmail.com>
2026-04-26 19:31:00 -0300

    chore: Upload simple development script

--- src/main.c
+++ src/main.c
@@ -1,5 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "avltree.h"
+
+void print_buffer(const int *buffer, int length);
+int compare_ints(const void *left, const void *right);
 
 int main(void)
 {
+    AvlTree *tree;
+
+    avltree_new(&tree, sizeof(int), compare_ints, NULL, NULL);
+
+    avltree_add(tree, &(int){4});
+    avltree_add(tree, &(int){2});
+    avltree_add(tree, &(int){1});
+    avltree_add(tree, &(int){0});
+    avltree_add(tree, &(int){3});
+    avltree_add(tree, &(int){6});
+    avltree_add(tree, &(int){5});
+    avltree_add(tree, &(int){7});
+    avltree_add(tree, &(int){8});
+
+    avltree_remove(tree, &(int){6});
+    avltree_remove(tree, &(int){5});
+    avltree_remove(tree, &(int){7});
+
+    avltree_add(tree, &(int){2});
+    avltree_add(tree, &(int){9});
+    avltree_add(tree, &(int){7});
+    avltree_add(tree, &(int){6});
+    avltree_add(tree, &(int){5});
+
+    size_t count;
+    avltree_getcount(tree, &count);
+    size_t size = count * sizeof(int);
+    int *buffer = malloc(size);
+
+    avltree_toarray(tree, buffer, size);
+
+    print_buffer(buffer, count);
+
+    bool exists;
+    avltree_exists(tree, &(int){0}, &exists);
+    printf("%d\n", exists);
+
+    avltree_exists(tree, &(int){-1}, &exists);
+    printf("%d\n", exists);
+
+    void *value;
+    avltree_get(tree, &(int){6}, &value);
+    printf("%d\n", *(int *)value);
+
+    free(buffer);
+
+    avltree_free(tree);
+
     return 0;
+}
+
+void print_buffer(const int *buffer, int length)
+{
+    printf("[");
+    if (length > 0)
+    {
+        printf(" %d", buffer[0]);
+    }
+    for (int i = 1; i < length; i++)
+    {
+        printf(", %d", buffer[i]);
+    }
+    printf(" ]\n");
+}
+
+int compare_ints(const void *left, const void *right)
+{
+    int l = *(int *)left;
+    int r = *(int *)right;
+    return (l > r) - (l < r);
 }
