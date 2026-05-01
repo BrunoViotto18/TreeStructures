087250de1d2baaa1799c83ceb4bbfdd700540742
BruhhnoV <brunoviottosantos@gmail.com>
2026-05-01 14:00:25 -0300

    feat: update to avl tree

--- src/main.c
+++ src/main.c
@@ -5,12 +5,12 @@
 
 void print_buffer(const int *buffer, int length);
 int compare_ints(const void *left, const void *right);
+bool filter(const void *value);
 
 int main(void)
 {
     AvlTree *tree;
-
-    avltree_new(&tree, sizeof(int), compare_ints, NULL, NULL);
+    avltree_new(&tree, sizeof(int), compare_ints, NULL);
 
     avltree_add(tree, &(int){4});
     avltree_add(tree, &(int){2});
@@ -22,15 +22,16 @@ int main(void)
     avltree_add(tree, &(int){7});
     avltree_add(tree, &(int){8});
 
-    avltree_remove(tree, &(int){6});
-    avltree_remove(tree, &(int){5});
-    avltree_remove(tree, &(int){7});
+    avltree_remove(tree, &(int){6}, NULL);
+    avltree_remove(tree, &(int){5}, NULL);
+    avltree_remove(tree, &(int){7}, NULL);
 
     avltree_add(tree, &(int){2});
     avltree_add(tree, &(int){9});
     avltree_add(tree, &(int){7});
     avltree_add(tree, &(int){6});
     avltree_add(tree, &(int){5});
+    avltree_add(tree, &(int){-1});
 
     size_t count;
     avltree_getcount(tree, &count);
@@ -48,13 +49,28 @@ int main(void)
     avltree_exists(tree, &(int){-1}, &exists);
     printf("%d\n", exists);
 
-    void *value;
+    int value;
     avltree_get(tree, &(int){6}, &value);
-    printf("%d\n", *(int *)value);
+    printf("%d\n", value);
+
+    AvlTreeIterator *iterator;
+    avltree_iterator_new(tree, &iterator, filter);
+
+    int value2;
+    while (avltree_iterator_next(iterator, &value2) == AVLTREE_OK)
+    {
+        printf("%d\n", value2);
+
+        avltree_iterator_getvalue(iterator, &value2);
+
+        printf("%d\n", value2);
+    }
+
+    avltree_iterator_free(iterator);
 
     free(buffer);
 
-    avltree_free(tree);
+    avltree_free(tree, NULL);
 
     return 0;
 }
@@ -78,4 +94,13 @@ int compare_ints(const void *left, const void *right)
     int l = *(int *)left;
     int r = *(int *)right;
     return (l > r) - (l < r);
+}
+
+bool filter(const void *value)
+{
+    return true;
+
+    int v = *(int *)value;
+
+    return v > 3 && v < 8;
 }
