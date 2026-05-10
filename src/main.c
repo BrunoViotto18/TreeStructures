805ae62da0f3ea2ac1cf447792ae63d02fe94877
Bruno Viotto dos Santos <Bruno.Santos03@cs.up.edu.br>
2026-05-10 20:54:41 -0300

    chore: Update for red black tree

--- src/main.c
+++ src/main.c
@@ -1,76 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "avltree.h"
+#include "rbtree.h"
 
 void print_buffer(const int *buffer, int length);
 int compare_ints(const void *left, const void *right);
-bool filter(const void *value);
 
 int main(void)
 {
-    AvlTree *tree;
-    avltree_new(&tree, sizeof(int), true, compare_ints, NULL);
+    RbTree *tree;
+    rbtree_new(&tree, sizeof(int), true, compare_ints, NULL);
 
-    avltree_add(tree, &(int){4});
-    avltree_add(tree, &(int){2});
-    avltree_add(tree, &(int){1});
-    avltree_add(tree, &(int){0});
-    avltree_add(tree, &(int){3});
-    avltree_add(tree, &(int){6});
-    avltree_add(tree, &(int){5});
-    avltree_add(tree, &(int){7});
-    avltree_add(tree, &(int){8});
+    rbtree_add(tree, &(int){4});
+    rbtree_add(tree, &(int){2});
+    rbtree_add(tree, &(int){1});
+    rbtree_add(tree, &(int){0});
+    rbtree_add(tree, &(int){3});
+    rbtree_add(tree, &(int){6});
+    rbtree_add(tree, &(int){5});
+    rbtree_add(tree, &(int){7});
+    rbtree_add(tree, &(int){8});
 
-    avltree_remove(tree, &(int){6}, NULL);
-    avltree_remove(tree, &(int){5}, NULL);
-    avltree_remove(tree, &(int){7}, NULL);
+    rbtree_remove(tree, &(int){6}, NULL);
+    rbtree_remove(tree, &(int){5}, NULL);
+    rbtree_remove(tree, &(int){7}, NULL);
 
-    avltree_add(tree, &(int){2});
-    avltree_add(tree, &(int){9});
-    avltree_add(tree, &(int){7});
-    avltree_add(tree, &(int){6});
-    avltree_add(tree, &(int){5});
-    avltree_add(tree, &(int){-1});
+    rbtree_add(tree, &(int){2});
+    rbtree_add(tree, &(int){9});
+    rbtree_add(tree, &(int){7});
+    rbtree_add(tree, &(int){6});
+    rbtree_add(tree, &(int){5});
 
     size_t count;
-    avltree_getcount(tree, &count);
+    rbtree_getcount(tree, &count);
     size_t size = count * sizeof(int);
     int *buffer = malloc(size);
 
-    avltree_toarray(tree, buffer, size);
+    rbtree_toarray(tree, buffer, size);
 
     print_buffer(buffer, count);
 
     bool exists;
-    avltree_exists(tree, &(int){0}, &exists);
+    rbtree_exists(tree, &(int){0}, &exists);
     printf("%d\n", exists);
 
-    avltree_exists(tree, &(int){-1}, &exists);
+    rbtree_exists(tree, &(int){-1}, &exists);
     printf("%d\n", exists);
 
     int value;
-    avltree_get(tree, &(int){6}, &value);
+    rbtree_get(tree, &(int){6}, &value);
     printf("%d\n", value);
 
-    AvlTreeIterator *iterator;
-    avltree_iterator_new(tree, &iterator, filter);
+    RbTreeIterator *iterator;
+    rbtree_iterator_new(tree, &iterator);
 
     int value2;
-    while (avltree_iterator_next(iterator, &value2) == AVLTREE_OK)
+    while (rbtree_iterator_next(iterator, &value2) == RBTREE_OK)
     {
+        // if (value2 <= 3 || value2 >= 8)
+        // {
+        //     continue;
+        // }
+
         printf("%d\n", value2);
 
-        avltree_iterator_getvalue(iterator, &value2);
+        rbtree_iterator_getvalue(iterator, &value2);
 
         printf("%d\n", value2);
     }
 
-    avltree_iterator_free(iterator);
+    rbtree_iterator_free(iterator);
 
     free(buffer);
 
-    avltree_free(tree, NULL);
+    rbtree_free(tree, NULL);
 
     return 0;
 }
@@ -94,13 +97,4 @@ int compare_ints(const void *left, const void *right)
     int l = *(int *)left;
     int r = *(int *)right;
     return (l > r) - (l < r);
-}
-
-bool filter(const void *value)
-{
-    return true;
-
-    int v = *(int *)value;
-
-    return v > 3 && v < 8;
 }
